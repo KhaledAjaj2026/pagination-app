b12ef81e08d215dfcd821d1b4f6215d64982c4b3
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-06-04 19:38:09 -0700

    first commit

---
+++ src/components/List/list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './list.scss';
+
+function List() {
+    return (
+        <>
+            <h1 className="list-title">List</h1>
+        </>
+    )
+}
+
+export default List;
