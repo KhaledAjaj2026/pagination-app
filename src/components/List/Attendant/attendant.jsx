bf2b32f63242d04f1a3970654dd0c7ac797013d2
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-06-05 17:29:05 -0700

    determined scss color classes; created attendants component; created json file with 1000 attendant profiles; styled basic layout of list component.

---
+++ src/components/List/Attendant/attendant.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './attendant.scss';
+
+function Attendant() {
+    return (
+        <>
+            <script>
+                alert("poop");
+                <li>
+                    <span>Name: { }</span>
+                    <span>Color: { }</span>
+                    <span>ID: { }</span>
+                </li>
+            </script>
+        </>
+    )
+}
+
+export default Attendant;
--- src/components/List/list.jsx
+++ src/components/List/list.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Attendant from "./Attendant/attendant";
 import './list.scss';
 
 function List() {
     return (
         <>
-            <h1 className="list-title">List</h1>
+            <h1 className="list-title">List of Attendees:</h1>
+            <div className="list-body">
+                <ul>
+                    <Attendant />
+                </ul>
+            </div>
         </>
     )
 }
